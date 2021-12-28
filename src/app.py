6b25e1c0b28097462309f9a7cd9628ae963fd341
Christos G <ckg@users.noreply.github.com>
2021-12-28 11:29:18 +0200

    base code for blog with deleted books.html and modified app.py and index.hml

--- src/app.py
+++ src/app.py
@@ -5,22 +5,11 @@ app = Flask(__name__)
 
 @app.route ("/index/")
 @app.route ("/")
-@app.route ("/<name>")
-def root(name=None):
-    return render_template("index.html", myname=name)
-
-@app.route ("/books/")
-def books():
-    with open("src/mybooks.json") as json_file:
-        jlist = json.load(json_file)
-        # print(jlist) #prints to console
-    return render_template("books.html", jlist=jlist)
+def root():
+    return render_template("index.html")
 
 
-@app.route ("/hello/")
-@app.route("/hello/<int:id>")
-def hello(id=None):
-    return f"hello {id}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
