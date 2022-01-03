72698e5d52af25dd0c33bbf17a9a4776ff709d70
Christos G <ckg@users.noreply.github.com>
2022-01-03 16:45:55 +0200

    Create form validators for signup form

--- src/forms.py
+++ src/forms.py
@@ -1,10 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class SignupForm(FlaskForm):
-    username = StringField(label='Username')
-    email = StringField(label="email")
-    password = StringField(label="password")
-    password2 = StringField(label="Επιβεβαίωση password")
+    username = StringField(label='Username', 
+                            validators=[
+                               DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό"), 
+                               Length(min=3, max=15, message="Αυτό το πεδίο πρέπει να είναι από 3 έως 15 χαρακτήρες")
+                            ]
+                           )
+
+    email = StringField(label="email",
+                         validators=[
+                               DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό"), 
+                               Email(message="Παρακαλώ εισάγετε ένα σωστό email")
+                          ]
+                        )
+
+    password = StringField(label="password",
+                            validators=[
+                               DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό"), 
+                               Length(min=3, max=15, message="Αυτό το πεδίο πρέπει να είναι από 3 έως 15 χαρακτήρες")
+                            ] 
+                           )
+
+    password2 = StringField(label="Επιβεβαίωση password",
+                             validators=[
+                               DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό"), 
+                               Length(min=3, max=15, message="Αυτό το πεδίο πρέπει να είναι από 3 έως 15 χαρακτήρες"),
+                               EqualTo("password", message="Τα password πρέπει να ταιριάζουν")
+                             ]
+                            )
+
     submit = SubmitField(label="Εγγραφή")
